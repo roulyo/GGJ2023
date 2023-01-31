18433812fa17e520ad7fc479ac0b1e453287d14a
Hach7ko <65307645+Hach7ko@users.noreply.github.com>
2023-02-01 00:03:03 +0100

    Clicking on Sprite opens game
    For now game only has two buttons (non working) and a title.

--- scripts/Sprite2D.cs
+++ scripts/Sprite2D.cs
@@ -1,7 +1,12 @@
 using Godot;
 
-public class ClickableSprite : Sprite
+public class Sprite2D : Sprite
 {
+    [Export]
+    public PackedScene MiniGame;
+    
+    private bool IsMiniGameOpen = false;
+
     public override void _Input(InputEvent inputEvent)
     {
         if(inputEvent is InputEventMouseButton) 
@@ -9,9 +14,11 @@ public class ClickableSprite : Sprite
             if(IsPixelOpaque(GetLocalMousePosition()))
             {
                 GetTree().SetInputAsHandled();
-                if(inputEvent.IsActionPressed("click"))
+                if(inputEvent.IsActionPressed("click") && !IsMiniGameOpen)
                 {
                     Modulate = new Color(0, 0, 1);
+                    IsMiniGameOpen = true;
+                    AddChild(MiniGame.Instance());
                 }
                 else
                 {
