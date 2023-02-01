d31285d7a3872319ffd325ee8286d9e91392d748
Roulyo <roulyo.42@gmail.com>
2023-02-01 17:08:24 +0100

    adds blur shader + disables button during minigames

--- scripts/ObjectOfInterest.cs
+++ scripts/ObjectOfInterest.cs
@@ -2,12 +2,17 @@ using Godot;
 
 public class ObjectOfInterest : Area2D
 {
+    [Signal]
+    public delegate void ObjectPressed();
+    [Signal]
+    public delegate void ObjectReleased();
+
     private bool IsMiniGameOpen = false;
 
 //-----------------------------------------------------------------------------
     public override void _Input(InputEvent inputEvent)
     {
-        if(inputEvent is InputEventMouseButton) 
+        if(inputEvent is InputEventMouseButton)
         {
             var sprite = GetNode<Sprite>("Sprite");
             if(sprite.IsPixelOpaque(GetLocalMousePosition()) && !IsMiniGameOpen)
@@ -41,5 +46,22 @@ public class ObjectOfInterest : Area2D
     private void OnArea2DMouseExited()
     {
         (GetNode<Sprite>("Sprite").Material as ShaderMaterial).SetShaderParam("width", 0.00);
+    }
+
+    //-----------------------------------------------------------------------------
+    private void _on_ObjectOfInterest_input_event(Node viewport, InputEvent @evt, int shape_idx)
+    {
+
+        if (@evt is InputEventMouseButton evtBttn)
+        {
+            if (evtBttn.Pressed)
+            {
+                EmitSignal(nameof(ObjectPressed));
+            }
+            else
+            {
+                EmitSignal(nameof(ObjectReleased));
+            }
+        }
     }
 }
