c9a5422fed5895b0a1d5bede7efe715e2fd048d2
Hach7ko <65307645+Hach7ko@users.noreply.github.com>
2023-02-01 12:42:25 +0100

    Fake Mini Game and Clickable Sprite
    When clicking on an object of interest, a mini game opens with a win / lose possibility. Player can close the game, play the game. If they loose they can retry, otherwise the clue is being shown.

--- scripts/Sprite2D.cs
+++ scripts/Sprite2D.cs
@@ -1,31 +1,30 @@
 using Godot;
 
 public class Sprite2D : Sprite
-{
-    [Export]
-    public PackedScene MiniGame;
-    
+{    
     private bool IsMiniGameOpen = false;
 
     public override void _Input(InputEvent inputEvent)
     {
         if(inputEvent is InputEventMouseButton) 
         {
-            if(IsPixelOpaque(GetLocalMousePosition()))
+            if(IsPixelOpaque(GetLocalMousePosition()) && !IsMiniGameOpen)
             {
                 GetTree().SetInputAsHandled();
-                if(inputEvent.IsActionPressed("click") && !IsMiniGameOpen)
+                if(inputEvent.IsActionPressed("click"))
                 {
-                    Modulate = new Color(0, 0, 1);
                     IsMiniGameOpen = true;
-                    AddChild(MiniGame.Instance());
-                }
-                else
-                {
-                    Modulate = new Color(1, 1, 1);
+                    GetNode<Node2D>("MiniGame").Show();
                 }
             }
         }
+    }
+
+    public void CloseMiniGame()
+    {
+        //TODO: Be sure to reset the game or not if keeping clue
+        IsMiniGameOpen = false;
+        GetNode<Node2D>("MiniGame").Hide();
     }
 
 }
