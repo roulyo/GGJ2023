586da10b964c7faecb1e59508ab61e8b5ec9a6ff
Roulyo <roulyo.42@gmail.com>
2023-02-01 12:47:59 +0100

    adds smooth room switch

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -3,56 +3,104 @@ using System;
 
 public class Main : Node2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    private const float MOVE_SPEED_PPS = 1024.0f;
 
-//#pragma warning disable 649
-    private CanvasItem[] _rooms = new CanvasItem[2];
-    private int _currentRoomIndex = 0;
-//#pragma warning restore 649
+    enum Direction : int
+    {
+        Left = -1,
+        Right = 1
+    }
 
-    // Called when the node enters the scene tree for the first time.
+    private Vector2     _screenSize;
+
+    private Node2D[]    _rooms = new Node2D[2];
+    private int         _currentRoomIndex = 0;
+
+    private bool        _switching = false;
+    private int         _moveDirection = 0;
+
+//-----------------------------------------------------------------------------
     public override void _Ready()
     {
-        _rooms[0] = GetNode<CanvasItem>("Room_Left");
-        _rooms[1] = GetNode<CanvasItem>("Room_Right");
+        _screenSize = GetViewportRect().Size;
+
+        _rooms[0] = GetNode<Node2D>("Room_Left");
+        _rooms[1] = GetNode<Node2D>("Room_Right");
 
         _rooms[_currentRoomIndex].Visible = true;
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//
-//  }
-
 //-----------------------------------------------------------------------------
-    private void GoToLeftRoom()
+    public override void _Process(float delta)
     {
-        _rooms[_currentRoomIndex].Visible = false;
-        _currentRoomIndex = nimod(--_currentRoomIndex, _rooms.Length);
-        _rooms[_currentRoomIndex].Visible = true;
+        if (_switching)
+        {
+            UpdateRoomSwitch(delta);
+        }
     }
 
 //-----------------------------------------------------------------------------
-    private void GoToRightRoom()
+    private void GoToRoom(Direction direction)
     {
-        _rooms[_currentRoomIndex].Visible = false;
-        _currentRoomIndex = nimod(++_currentRoomIndex, _rooms.Length);
+        _switching = true;
+        _moveDirection = (int)direction;
+        _currentRoomIndex = nimod(_currentRoomIndex + (int)direction, _rooms.Length);
         _rooms[_currentRoomIndex].Visible = true;
+        _rooms[_currentRoomIndex].Position = new Vector2(
+            x: _screenSize.x * _moveDirection,
+            y: 0
+        );
+
+        SetMoveButtonEnabled(false);
+    }
+
+//-----------------------------------------------------------------------------
+    private void UpdateRoomSwitch(float delta)
+    {
+        int oldRoomIndex = nimod(_currentRoomIndex - _moveDirection, _rooms.Length);
+        float xTranslation = MOVE_SPEED_PPS * delta * -_moveDirection;
+        bool switchCompleted = false;
+
+        _rooms[oldRoomIndex].MoveLocalX(xTranslation);
+        _rooms[_currentRoomIndex].MoveLocalX(xTranslation);
+
+        if (   (_moveDirection > 0 && _rooms[_currentRoomIndex].Position.x < 0)
+            || (_moveDirection < 0 && _rooms[_currentRoomIndex].Position.x > 0))
+        {
+            _rooms[_currentRoomIndex].Position = new Vector2(
+                x: 0,
+                y: 0
+            );
+            switchCompleted = true;
+        }
+
+        if (switchCompleted)
+        {
+            _rooms[oldRoomIndex].Visible = false;
+            _moveDirection = 0;
+            _switching = false;
+
+            SetMoveButtonEnabled(true);
+        }
+    }
+
+//-----------------------------------------------------------------------------
+    private void SetMoveButtonEnabled(bool enabled)
+    {
+        GetNode<Button>("Control/GoToLeftButton").Disabled = !enabled;
+        GetNode<Button>("Control/GoToRightButton").Disabled = !enabled;
     }
 
 //-----------------------------------------------------------------------------
     private void _on_GoToLeftButton_pressed()
     {
-        GoToLeftRoom();
+        GoToRoom(Direction.Left);
     }
 
 //-----------------------------------------------------------------------------
     private void _on_GoToRightButton_pressed()
     {
-        GoToRightRoom();
+        GoToRoom(Direction.Right);
     }
 
 //-----------------------------------------------------------------------------
